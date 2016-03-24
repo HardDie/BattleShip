0fe6bba6462a173a6ca3760d097071319f33fa4f
HardDie <oleg1995sysoev@yandex.ru>
2016-03-24 22:19:17 +0600

    Подключение библиотек для создание TCP сокетов

--- net.h
+++ net.h
@@ -1,7 +1,12 @@
 #ifndef _NET_H_
 #define _NET_H_
 
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
 #include <ncurses.h>
+#include <netdb.h>
 #include "draw.h"
 
 void    net_setup( const char typeConnection );
