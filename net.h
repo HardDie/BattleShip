6f848ea90a2108a8ae859d94168df4f388b1dc70
HardDie <oleg1995sysoev@yandex.ru>
2016-03-24 23:35:57 +0600

    Добавлены заголовочные файлы для функции kill

--- net.h
+++ net.h
@@ -1,12 +1,14 @@
 #ifndef _NET_H_
 #define _NET_H_
 
+#define _POSIX_SOURCE
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <strings.h>
 #include <ncurses.h>
 #include <netdb.h>
+#include <signal.h>
 #include "draw.h"
 
 void    net_setup( const char typeConnection );
